de73cd34417ba4ec9007e1abb1690ddb39af6831
Ethan Buchanan <ethan@buchananville.com>
2022-03-24 13:32:15 -0700

    added basic infrastructure so server.js can be run

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+
+// For future referance
+/*
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+*/
+
+module.exports = router;
